abd893eade91231a82a25076b150e3b1592c43a9
Paul <paul@niobium.me>
2017-02-19 16:40:30 +0100

    Top level commands now working.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 import createConfig from './config'
 
-let config = null
+var config = null
+var commands = {}
+
+const incomingMiddleware = (event, next) => {
+  if (event.type !== 'message' && event.type !== 'text') {
+    console.log('wai')
+    return next()
+  }
+
+  const rawmsg = event.text
+  if (!rawmsg.startsWith(config.prefix.get())) {
+    return next()
+  }
+  const msg = rawmsg.slice(config.prefix.get().length)
+  const cmd = msg.split(' ')[0]
+  const args = msg.split(' ').slice(1)
+  if (commands[cmd] === null) {
+    return next()
+  }
+  commands[cmd](event, next, args)
+}
 
 module.exports = {
   init: function (bp) {
-    console.log(config)
+    bp.middlewares.register({
+      name: 'commands.handle',
+      type: 'incoming',
+      order: 100,
+      handler: incomingMiddleware,
+      module: 'botpress-simplecommands',
+      description: 'Parses commands and stuff.'
+    })
+    bp.addCommand = (name, handler) => {
+      commands[name] = handler
+    }
   },
 
   ready: function (bp) {
     config = createConfig(bp)
-    bp.addCommand = (name, handler) => {
-
-    }
   }
 }
